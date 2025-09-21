9f6e4c70c3ad4cf63b474af7d98a520f5f1a0d08
Lordpun <120076268+Lordpun@users.noreply.github.com>
2025-09-21 00:35:52 -0400

    Changed the system for showing/hiding popus to be a state system

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
 const app = Vue.createApp({
   template: `
   <header>
-    <h1>Text Script Recorder</h1>
+    <h1>Text Recorder</h1>
   </header>
   
   <main>
-    <button>Add Name</button>
+    <button @click="this.popupState = 'add'">Add Name</button>
     <button>Remove Name</button>
     <button>View Text</button>
     <button>Add Line</button>
     <button id="send">Send to Drive</button>
     
-    <popupWin>
-      <template #title><h3>Test</h3></template>
+    <!-- Popups -->
+    
+    <!-- Name -->
+    <popupWin v-if="popupState == 'add'" @close="closePopup">
+      <template #title><h3>Add a name reference</h3></template>
+      
+      <template #input><p>Placeholder</p></template>
     </popupWin>
   </main>
-  `
+  `,
+  
+  data() {
+    return {
+      popupState: "none"
+    };
+  },
+  
+  methods: {
+    closePopup() {
+      this.popupState = "none";
+    },
+  }
 });
 
 app.component("popupWin", popup);
--- popup.js
+++ popup.js
@@ -1,24 +1,19 @@
 const popup = {
   template: `
-  <div v-show="show" ref="popup" class="popupBackground">
+  <div class="popupBackground">
     <article class="popupBody">
       <slot name="title"></slot>
       <slot name="output"></slot>
       <slot name="input"></slot>
       
-      <button @click="closePopup">Exit</button>
+      <button @click="closeEvent">Exit</button>
     </article>
   </div>
   `,
-  data() {
-    return {
-      show: false
-    };
-  },
   
   methods: {
-    closePopup() {
-      this.show = false;
+    closeEvent() {
+      this.$emit('close');
     }
   }
 };
